dc4ebf860341bed979574c4388ed007e28974ae1
94810 <pablo.bourdelas@gmail.com>
2017-07-19 15:28:44 +0200

    Commit en spd

---
+++ src/Unity/Sweet Spine/Assets/CollideHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideHandler : MonoBehaviour, IWinDetection {
+	#region WinDetection implementation
+
+	public bool IsGood { get; set;}
+
+
+	#endregion
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	void OnTriggerEnter(Collider obj){
+		EndGameDetection scrtPrt = this.transform.parent.GetComponent<EndGameDetection> ();
+
+		if (obj.gameObject.tag == this.tag)
+			IsGood = true;
+		else IsGood = false ;
+		scrtPrt.CheckEndGame ();
+	}
+
+	void OnTriggerExit(Collider obj){
+		IsGood = false;
+	}
+}
---
+++ src/Unity/Sweet Spine/Assets/IMovuino.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovuino{
+	bool status { get;} // Online / Offline
+	MoveL mvt { get;} 
+}
