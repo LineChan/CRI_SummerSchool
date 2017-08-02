af3c932b3623b5bdcbfbd12cdbfd55ca00a3481b
Aymeric Faucher <aymeric.faucher@gmail.com>
2017-08-02 21:17:57 +0200

    Just a door

--- src/Unity/Sweet Spine/Assets/Scripts/TempleIWin/EndGameActivateAnimationTrigger.cs
+++ src/Unity/Sweet Spine/Assets/Scripts/TempleIWin/EndGameActivateAnimationTrigger.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndGameActivateAnimationTrigger : EndGameDetection {
-	public string triggerName = "EndGame";
+	public string triggerName = "";
+	public string booleanName = "";
+	public bool booleanValue = false;
 	public override void EndGameAction ()
 	{
 		base.EndGameAction ();
 		Debug.Log (triggerName);
-		GetComponent<Animator> ().SetTrigger (triggerName);
+		if (triggerName != "")
+			GetComponent<Animator> ().SetTrigger (triggerName);
+		if (booleanName != "")
+			GetComponent<Animator> ().SetBool (booleanName, booleanValue);
 	}
 }
