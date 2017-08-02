b966fd000b4a9d694020a0b8fb3ef687041eda06
94810 <pablo.bourdelas@gmail.com>
2017-08-02 13:03:15 +0200

    Enigm seconde salle

---
+++ src/Unity/Sweet Spine/Assets/LeverEnigm.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverEnigm : MonoBehaviour, IWinDetection {
+	
+	#region IWinDetection implementation
+	public bool IsGood {
+		get {
+			return _IsGood;
+		}
+	}
+	#endregion
+
+	public List<bool> soluce;
+	public List<GameObject> levers ;
+	public bool InitState; 
+
+	private bool _IsGood;
+
+	// Use this for initialization
+	void Start () {
+		
+		_IsGood = InitState ;
+	}
+
+	public void CheckEnigmState(){
+		_IsGood = true;
+		for (int i = 0; i < levers.Count; i++) {
+			if (levers [i].GetComponent<SwitchHandler> ().status != soluce [i]) {
+				_IsGood = false;
+				Debug.Log ("Lever Not Ok");
+			}
+				
+		}
+
+		this.GetComponentInParent<EndGameDetection> ().CheckEndGame ();
+	}
+}
---
+++ src/Unity/Sweet Spine/Assets/SwitchHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHandler : MonoBehaviour, ICursorAction {
+	#region ICursorAction implementation
+	public CursorAction action {
+		get {
+			return CursorAction.Use;
+		}
+	}
+	#endregion
+
+	public bool status;
+
+	private Vector3 temp;
+
+	void On(){
+		temp = this.transform.position;
+		temp.x = 15.69f;
+		this.transform.position = temp;
+	}
+
+	void Off(){
+		temp = this.transform.position;
+		temp.x = 14.09f;
+		this.transform.position = temp;
+	}
+
+	void Start(){
+		if(status) On();
+		else Off();
+	}
+
+	public void toggle(){
+		if (status) {
+			status = false;
+			Off();
+		} else {
+			status = true;
+			On();
+		}
+	}
+}
