4f4e805fc7939206e1d07177be327ce5afe5395d
Aymeric Faucher <aymeric.faucher@gmail.com>
2017-08-07 15:29:35 +0200

    Prefab updated

---
+++ src/Unity/Sweet Spine/Assets/MovuinoSensorUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Movuino;
+
+public class MovuinoSensorUI : MonoBehaviour {
+	public enum MovuinoSensorUIType
+	{
+		Accelerometer,
+		Gyroscope,
+		Magnetometer,
+	}
+	public Text xText;
+	public Text yText;
+	public Text zText;
+	public MovuinoSensorUIType sensorType;
+
+	public void Update()
+	{
+		Stack<MovuinoSensorData> sensorData = MovuinoManager.Instance.GetLog<MovuinoSensorData> ("/movuinoOSC");
+
+	}
+}
