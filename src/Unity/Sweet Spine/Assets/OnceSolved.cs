9f4f97fa332cdf3295447fb8c768e2b1e68b68d1
94810 <pablo.bourdelas@gmail.com>
2017-08-08 10:39:52 +0200

    Third Level

---
+++ src/Unity/Sweet Spine/Assets/OnceSolved.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnceSolved : MonoBehaviour {
+
+	private Enigm e;
+	public string sceneToLoad ;
+
+	void Start(){
+		e=this.GetComponent<Enigm> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(e.solve)
+			SceneManager.LoadScene (sceneToLoad);
+	}
+}
---
+++ src/Unity/Sweet Spine/Assets/WorldToggle.cs
@@ -0,0 +1,34 @@
+﻿   using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldToggle : MonoBehaviour {
+
+	private bool KillSwitch ;
+
+	// Use this for initialization
+	void Start () {
+		PlayerController.onChangeWorld += ToggleMamen ;
+		KillSwitch = false;
+	}
+
+	public void ToggleKillSwitch(){
+		KillSwitch = !KillSwitch;
+	}
+		
+	void ToggleMamen (World world){
+		
+		if(KillSwitch){
+			this.GetComponentInParent<Lever> ().Toggle ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDisable(){
+		PlayerController.onChangeWorld -= ToggleMamen;
+	}
+}
